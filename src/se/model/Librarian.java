508909f2dc94a306c2a776f4608dbe9e8d092f07
Adde86 <57154481+Adde86@users.noreply.github.com>
2020-04-21 13:06:46 +0200

    added entities for admin, guest and librarian

---
+++ src/se/model/Librarian.java
@@ -0,0 +1,74 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package se.model;
+
+/**
+ *
+ * Log in with personId and password
+ */
+public class Librarian {
+    
+    private int id;
+    
+    private String firstName;
+    
+    private String lastName;
+    
+    private String personId;
+    
+    private String password;
+
+    public Librarian(int id, String firstName, String lastName, String personId, String password) {
+        this.id = id;
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.personId = personId;
+        this.password = password;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
+
+    public String getPersonId() {
+        return personId;
+    }
+
+    public void setPersonId(String personId) {
+        this.personId = personId;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+    
+    
+    
+}
