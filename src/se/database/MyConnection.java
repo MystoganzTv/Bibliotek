970e93e27dbf6985509a0df988f8647f3a3099a8
annazamarou <annazamarou@gmail.com>
2020-05-04 11:23:27 +0200

    delete methoder för böcker och e-böcker
    klar

--- src/se/database/MyConnection.java
+++ src/se/database/MyConnection.java
@@ -14,8 +14,12 @@ public class MyConnection {
     
         try{
         // get connection to database
-        String url ="jdbc:mysql://libsys.mysql.database.azure.com:3306/lib_db?useSSL=true&requireSSL=false"; 
-        myDbConn = DriverManager.getConnection(url, "java@libsys", "Projekt!");
+        //String url ="jdbc:mysql://libsys.mysql.database.azure.com:3306/lib_db?useSSL=true&requireSSL=false"; 
+        //myDbConn = DriverManager.getConnection(url, "java@libsys", "Projekt!");
+        
+        //connection tilll lokalt databas
+        Class.forName("com.mysql.jdbc.Driver");
+        myDbConn=DriverManager.getConnection("jdbc:mysql://localhost:3306/lib_db","root","Xouniou13");
         if(myDbConn != null)
         {
             
