a4276f7b0c8286ecd626fa3ba4af7fc9d8312248
dannycalos <dannycalos@outlook.com>
2020-05-16 12:06:35 +0200

    local connection fixed

--- src/se/database/MyConnection.java
+++ src/se/database/MyConnection.java
@@ -14,9 +14,11 @@ public class MyConnection {
     
         try{
 
-            String url ="jdbc:mysql://libsys.mysql.database.azure.com:3306/libsys?useSSL=true&requireSSL=false"; 
-            myDbConn = DriverManager.getConnection(url, "java@libsys", "Projekt!");        
-
+      //    String url = "jdbc:mysql://libsys.mysql.database.azure.com:3306/libsys?useSSL=true&requireSSL=false";
+      //    myDbConn = DriverManager.getConnection(url, "java@libsys", "Projekt!");
+      
+            String url ="jdbc:mysql://localhost:3306/library?useSSL=true&requireSSL=false";
+            myDbConn = DriverManager.getConnection(url, "root", "root");
             return myDbConn;
         
         }catch(Exception e)
