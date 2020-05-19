1d38ba6a60ac0254b47f5ef6d07c2980d0a6f1a6
Adde86 <57154481+Adde86@users.noreply.github.com>
2020-05-19 09:53:59 +0200

    fixed eBook model

--- src/se/view/AddBook.java
+++ src/se/view/AddBook.java
@@ -370,7 +370,7 @@ public class AddBook extends javax.swing.JFrame {
             Books b = new Books(title, author, isbn, publisher, purchasePrice, category, placement, inStock, desc);
             qMethods.addBook(b);
         } else {
-            E_Books b = new E_Books(title, author, isbn, publisher, purchasePrice, category, placement, desc);
+            E_Books b = new E_Books(title, author, isbn, publisher, purchasePrice, category,desc);
             qMethods.addEBook(b);
         }
 
