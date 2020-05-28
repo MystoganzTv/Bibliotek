95d62bbb01e7422c68874130dc72c79d30703442
Adde86 <57154481+Adde86@users.noreply.github.com>
2020-05-28 11:19:21 +0200

    changed name on view for upcoming seminars

--- src/se/view/ViewUpcomingSeminarsView.java
+++ src/se/view/ViewUpcomingSeminarsView.java
@@ -14,7 +14,7 @@ import se.model.Seminar;
  *
  * @author adde
  */
-public class SeminarsView extends javax.swing.JFrame {
+public class ViewUpcomingSeminarsView extends javax.swing.JFrame {
     
     private QueryMethods queryMethods = new QueryMethods();
     private DefaultListModel model = new DefaultListModel();
@@ -24,7 +24,7 @@ public class SeminarsView extends javax.swing.JFrame {
     /**
      * Creates new form SeminasView
      */
-    public SeminarsView() {
+    public ViewUpcomingSeminarsView() {
         initComponents();
         
         setLocationRelativeTo(null);
@@ -271,21 +271,23 @@ public class SeminarsView extends javax.swing.JFrame {
                 }
             }
         } catch (ClassNotFoundException ex) {
-            java.util.logging.Logger.getLogger(SeminarsView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ViewUpcomingSeminarsView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (InstantiationException ex) {
-            java.util.logging.Logger.getLogger(SeminarsView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ViewUpcomingSeminarsView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (IllegalAccessException ex) {
-            java.util.logging.Logger.getLogger(SeminarsView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ViewUpcomingSeminarsView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         } catch (javax.swing.UnsupportedLookAndFeelException ex) {
-            java.util.logging.Logger.getLogger(SeminarsView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
+            java.util.logging.Logger.getLogger(ViewUpcomingSeminarsView.class.getName()).log(java.util.logging.Level.SEVERE, null, ex);
         }
+        //</editor-fold>
+        //</editor-fold>
         //</editor-fold>
         //</editor-fold>
 
         /* Create and display the form */
         java.awt.EventQueue.invokeLater(new Runnable() {
             public void run() {
-                new SeminarsView().setVisible(true);
+                new ViewUpcomingSeminarsView().setVisible(true);
             }
         });
     }
